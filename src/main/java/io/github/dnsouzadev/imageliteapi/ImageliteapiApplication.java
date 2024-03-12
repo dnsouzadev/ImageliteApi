cdd67e4533fe0c920b74c35965827fea03f8bba3
Daniel Souza <workdndsza@gmail.com>
2024-03-12 19:39:15 -0300

    setting the project

---
+++ src/main/java/io/github/dnsouzadev/imageliteapi/ImageliteapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.dnsouzadev.imageliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageliteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageliteapiApplication.class, args);
+	}
+
+}
