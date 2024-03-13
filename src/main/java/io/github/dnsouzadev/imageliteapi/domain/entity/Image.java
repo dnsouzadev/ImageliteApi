44bd6d05654e08dae9d5914b0eca5a93e608c8ce
Daniel Souza <workdndsza@gmail.com>
2024-03-12 21:17:44 -0300

    creating the JPA mapping for Image entity

---
+++ src/main/java/io/github/dnsouzadev/imageliteapi/domain/entity/Image.java
@@ -0,0 +1,42 @@
+package io.github.dnsouzadev.imageliteapi.domain.entity;
+
+import io.github.dnsouzadev.imageliteapi.domain.enums.ImageExtension;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String name;
+    @Column
+    private Long size;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ImageExtension extension;
+    @Column
+    @CreatedDate
+    private LocalDateTime uploadDate;
+    @Column
+    private String tags;
+    @Column
+    @Lob
+    private byte[] file;
+}
---
+++ src/main/java/io/github/dnsouzadev/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package io.github.dnsouzadev.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    GIF,
+    JPEG
+}
